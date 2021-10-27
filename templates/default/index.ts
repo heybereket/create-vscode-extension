e081a78e5ec39c82e24094dd358d8ce857d73506
Bereket Semagn <semagnbereket@gmail.com>
2021-10-27 18:25:35 -0400

    feat: default template

--- templates/default/index.ts
+++ templates/default/index.ts
@@ -1,1 +1,15 @@
-console.log('default')
+import * as vscode from "vscode";
+
+// Runs when your extension is activated for the first time
+export const activate = (context: vscode.ExtensionContext): void => {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("foo", () => {
+      console.log("bar");
+    })
+  );
+};
+
+// Runs when the extension is deactivated
+export const deactivate = (): void => {
+  console.log("Deactivated Extension");
+};
